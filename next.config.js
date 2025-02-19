bbe6bb7011aee045956241cba47524dc73b00b2b
RishabhShrival <rishabhshrival746@gmail.com>
2025-02-20 00:16:22 +0530

    Change

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true, // Helps catch potential issues in React components
+  experimental: {}, // Used for enabling experimental Next.js features
+};
+
+module.exports = nextConfig;
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -102,4 +102,6 @@ const Home: React.FC = () => {
         </div>
     );
 
-}
+};
+
+export default Home;
